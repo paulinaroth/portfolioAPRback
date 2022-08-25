e6cc015b8f3f6c302a7e3b57ad3057433e9095cd
paulinaroth <rothermel.alejandra@gmail.com>
2022-08-25 03:53:35 -0300

    modificado de cors

--- src/main/java/com/alePaulina/api/ApiApplication.java
+++ src/main/java/com/alePaulina/api/ApiApplication.java
@@ -9,18 +9,18 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @SpringBootApplication
 public class ApiApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiApplication.class, args);
-	}
-        
-        @Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("https://portfolio-ap-35745.web.app").allowedMethods("*").allowedHeaders("*");
-			}
-		};
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ApiApplication.class, args);
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowedOrigins("https://portfolio-ap-35745.web.app").allowedMethods("GET", "POST", "PUT", "DELETE").allowedHeaders("*");
+            }
+        };
+    }
 
 }

dd9c772953a4e7e0f9c05f39ab412c8d50e1da39
paulinaroth <rothermel.alejandra@gmail.com>
2022-08-25 19:30:31 -0300

    quitando crossorigin

--- src/main/java/com/alePaulina/api/controllers/LocationController.java
+++ src/main/java/com/alePaulina/api/controllers/LocationController.java
@@ -9,7 +9,6 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,7 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Pauline
  */
-@CrossOrigin(origins = "*")
+
 @RestController
 public class LocationController {
     @Autowired
--- src/main/java/com/alePaulina/api/controllers/SkillController.java
+++ src/main/java/com/alePaulina/api/controllers/SkillController.java
@@ -24,7 +24,7 @@ import org.springframework.web.bind.annotation.RestController;
  *
  * @author Pauline
  */
-@CrossOrigin(origins = "*")
+
 @RestController
 public class SkillController {
     

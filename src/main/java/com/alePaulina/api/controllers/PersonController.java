b4e76230551e7456b61592be46dc8ecaccdd7330
paulinaroth <rothermel.alejandra@gmail.com>
2022-08-25 13:02:59 -0300

    agrego cors a personcontroller

--- src/main/java/com/alePaulina/api/controllers/PersonController.java
+++ src/main/java/com/alePaulina/api/controllers/PersonController.java
@@ -3,13 +3,13 @@ package com.alePaulina.api.controllers;
 
 import com.alePaulina.api.exception.NotFoundException;
 import com.alePaulina.api.models.Person;
-import com.alePaulina.api.repositories.ProvinceRepository;
 import com.alePaulina.api.services.IPersonService;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +20,9 @@ import org.springframework.web.bind.annotation.RestController;
 
 
 // @author Pauline
+
+
+@CrossOrigin(origins = "*")
 @RestController
 public class PersonController {
     
